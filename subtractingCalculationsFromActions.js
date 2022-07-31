e15db310cf00387b3f324f35e6052369c48dd292
ABlued <skwodhkswkd@naver.com>
2022-07-31 22:32:31 +0900

    feat : 세금을 계산하여 반영하는 updateTaxDom 함수 구현

--- subtractingCalculationsFromActions.js
+++ subtractingCalculationsFromActions.js
@@ -29,18 +29,17 @@ function clacTotal(cart) {
   return total;
 }
 
-/**
- * 이런 식으로 함수 내에 암묵적 입력, 출력을 제거하여 액션에서 계산을 빼내는 것이 좋다.
- * 이런 방식을 통해 함수는
- * 1. DOM 업데이트와 비즈니스 로직이 분리되었으며
- * 2. 함수는 더 이상 전역변수의 의존되지 않고
- * 3. 결괏값을 리턴하게 되었다.
- *
- * 이런 방식은 다음의 장점들이 있다.
- *
- * 1. 재사용성을 높인다. (액션보다는 계산이 여러 군데에서 사용될 가능성이 높다.)
- * 2. 쉽게 테스트할 수 있다.(계산은 함수의 호출시점, 호출한 수의 의존하지 않는다.)
- */
+function updateTaxDom() {
+  setTaxDom(clacTax(shoppingCartTotal));
+}
+
+function clacTax(amount) {
+  return amount * 0.1;
+}
+
+function setTaxDom() {
+  // 세금을 반영하기 위해 DOM 업데이트를 하는 코드...
+}
 
 function setCartTotalDom() {
   // 금액 합계를 반영하기 위해 DOM 업데이트를 하는 코드...
