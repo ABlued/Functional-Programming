2b41d9c30f1770abaaba1489875aced2424e5b1d
ABlued <skwodhkswkd@naver.com>
2022-08-07 18:30:24 +0900

    feat : 중복 제거 및 getsFreeShipping 기능 수정

--- subtractingCalculationsFromActions.js
+++ subtractingCalculationsFromActions.js
@@ -18,6 +18,8 @@ function addItem(cart, name, price) {
 function calcCartTotal() {
   shoppingCartTotal = clacTotal();
   setCartTotalDom();
+  updateShippingIcons(shoppingCart);
+  updateTaxDom();
 }
 
 function clacTotal(cart) {
@@ -25,18 +27,17 @@ function clacTotal(cart) {
   for (let i = 0; i < cart.length; i++) {
     const item = cart[i];
     total += item.price;
-    // 또한 중복된 코드가 있습니다. 합계에 제품 가격을 더하는 코드가 두 군데 있습니다.
   }
   return total;
 }
 
-function updateShippingIcons() {
+function updateShippingIcons(cart) {
   const buyButtons = getBuyButtonDom();
   for (let i = 0; i < buyButtons.length; i++) {
     const button = buyButtons[i];
     const item = button.item;
-    if (getsFreeShipping(shoppingCartTotal, item.price)) {
-      // Dom 업데이트 로직과 비즈니스 규칙이 분리되었다.
+    const newCart = addItem(cart, item.name, item.price);
+    if (getsFreeShipping(newCart)) {
       button.showFreeShoppingIcons();
     } else {
       button.hideFreeShoppingIcons();
@@ -44,14 +45,11 @@ function updateShippingIcons() {
   }
 }
 
-function getsFreeShipping(itemPrice, total) {
-  // 이 함수에는 더 이상 암묵적 입출력이 없지만 요구사항에 부합한 함수입니다.
-  /**
-   * 요구 사항은 장바구니에 담긴 제품을 주문할 때 무료 배송인지 확인하는 것인데
-   * 장바구니로 무료 배송을 확인하지 않고 제품의 합계와 가격으로 학인하고 있습니다.
-   * 이 함수는 비즈니스 요구 사항과 맞지 않는 인자라고 할 수 있습니다.
-   */
-  return itemPrice + total >= 20;
+function getsFreeShipping(cart) {
+  // 함수의 기능 자체를 바꾸었기 때문에 리팩토링이 아닙니다.
+  // 하지만 장바구니 내용을 이용하여 무료 배송인지 알려줍니다.
+  // 또한 중복되는 코드도 사라졌습니다.
+  return clacTotal(cart) >= 20;
 }
 
 function updateTaxDom() {
