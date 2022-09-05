9a0e3947e0aa2a55d17f6b9e935546d5205fedd1
ABlued <skwodhkswkd@naver.com>
2022-09-05 22:27:15 +0900

    refactor : 쓰면서 읽기도 하는 동작들 리팩터링

--- aFunctionThatReadsAndWrites.js
+++ aFunctionThatReadsAndWrites.js
@@ -18,3 +18,39 @@
  * 1. 읽기와 쓰기 함수로 각각 분리한다
  * 2. 함수에서 값을 두 개 분리한다.
  */
+
+// 1. 읽기와 쓰기 동작으로 분리하기
+
+function first_element(array) {
+  // 읽기(계산)
+  return array[0];
+}
+
+function drop_first(array) {
+  // 쓰기
+  const array_copy = array.slice();
+  array_copy.shift();
+  return array_copy;
+}
+
+// 이제 읽기와 쓰기를 선택해서 쓸 수 있다.
+
+// 2. 함수에서 값을 두 개를 리턴한다.
+
+function shift(array) {
+  const array_copy = array.slice();
+  const first = array_copy.shift();
+  return {
+    first: first,
+    array: array_copy,
+  };
+}
+
+// 혹은 이렇게도 쓸 수 있다.
+
+function shift2(array) {
+  return {
+    first: first_element(array),
+    array: drop_first(array),
+  };
+}
